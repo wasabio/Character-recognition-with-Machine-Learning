51adb81dc4c8920dd4e626ca10f81b00d074b3bf
wasabio <kerichardyann@gmail.com>
2018-11-16 12:04:53 -0500

    Adding neural networks

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,6 @@
-from sklearn.externals import joblib
 from sklearn import naive_bayes
 from sklearn import tree
+from sklearn.neural_network import MLPClassifier # Neural Network : MultiLayer Perceptron with back propagation
 from sklearn.metrics import accuracy_score
 import os
 import pickle
@@ -63,17 +63,23 @@ with open(os.path.join(path, "datasets", "ds2", "ds2Test.csv"), 'r') as file:
 # Reading csv files for DS1 and DS2 - END
 
 # Training classifier (option 1)
-classifierDT1 = tree.DecisionTreeClassifier()
+classifierDT1 = tree.DecisionTreeClassifier(criterion='entropy')
 classifierDT1.fit(featuresTrainDS1, labelsTrainDS1)
 
 classifierNB1 = naive_bayes.MultinomialNB(alpha=0.5)
 classifierNB1.fit(featuresTrainDS1, labelsTrainDS1)
 
-classifierDT2 = tree.DecisionTreeClassifier()
+classifierMLP1 = MLPClassifier(alpha=0.5, hidden_layer_sizes=(200, 80), random_state=1)
+classifierMLP1.fit(featuresTrainDS1, labelsTrainDS1)
+
+classifierDT2 = tree.DecisionTreeClassifier(criterion='entropy')
 classifierDT2.fit(featuresTrainDS2, labelsTrainDS2)
 
 classifierNB2 = naive_bayes.MultinomialNB(alpha=0.5)
 classifierNB2.fit(featuresTrainDS2, labelsTrainDS2)
+
+classifierMLP2 = MLPClassifier(alpha=0.5, hidden_layer_sizes=(200, 80), random_state=1)
+classifierMLP2.fit(featuresTrainDS2, labelsTrainDS2)
 # Training classifier - End
 
 # Loading an existing trained classifier (option 2)
@@ -101,6 +107,22 @@ write_results_file("ds1Val-nb.csv", val_predicted)
 write_results_file("ds1Test-nb.csv", test_predicted)
 save_model(classifierNB1, "ds1-nb.pkl")
 
+val_predicted = classifierMLP1.predict(featuresValDS1)
+accuracy = accuracy_score(labelsValDS1, val_predicted)
+print("Accuracy for DS1 with Neural Network : {}".format(accuracy))
+test_predicted = classifierMLP1.predict(featuresTestDS1)
+write_results_file("ds1Val-mlp.csv", val_predicted)
+write_results_file("ds1Test-mlp.csv", test_predicted)
+save_model(classifierMLP1, "ds1-mlp.pkl")
+
+val_predicted = classifierNB2.predict(featuresValDS2)
+accuracy = accuracy_score(labelsValDS2, val_predicted)
+print("Accuracy for DS2 with Naive Bayes : {}".format(accuracy))
+test_predicted = classifierNB2.predict(featuresTestDS2)
+write_results_file("ds2Val-nb.csv", val_predicted)
+write_results_file("ds2Test-nb.csv", test_predicted)
+save_model(classifierNB2, "ds2-nb.pkl")
+
 val_predicted = classifierDT2.predict(featuresValDS2)
 accuracy = accuracy_score(labelsValDS2, val_predicted)
 print("Accuracy for DS2 with Decision Tree : {}".format(accuracy))
@@ -109,11 +131,11 @@ write_results_file("ds2Val-dt.csv", val_predicted)
 write_results_file("ds2Test-dt.csv", test_predicted)
 save_model(classifierDT1, "ds2-dt.pkl")
 
-val_predicted = classifierNB2.predict(featuresValDS2)
+val_predicted = classifierMLP2.predict(featuresValDS2)
 accuracy = accuracy_score(labelsValDS2, val_predicted)
-print("Accuracy for DS2 with Naive Bayes : {}".format(accuracy))
-test_predicted = classifierNB2.predict(featuresTestDS2)
-write_results_file("ds2Val-nb.csv", val_predicted)
-write_results_file("ds2Test-nb.csv", test_predicted)
-save_model(classifierNB1, "ds2-nb.pkl")
+print("Accuracy for DS2 with Neural Network : {}".format(accuracy))
+test_predicted = classifierMLP2.predict(featuresTestDS2)
+write_results_file("ds2Val-mlp.csv", val_predicted)
+write_results_file("ds2Test-mlp.csv", test_predicted)
+save_model(classifierMLP2, "ds2-mlp.pkl")
 # Printing & saving - END
